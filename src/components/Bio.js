3be764bb9de90a47d9d94bbcfc9ed21fb7109336
Tim Butler <ketsuniku@gmail.com>
2018-11-22 11:45:58 -0500

    First commit

---
+++ src/components/Bio.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+// Import typefaces
+import 'typeface-montserrat'
+import 'typeface-merriweather'
+
+import profilePic from './profile-pic.jpg'
+import { rhythm } from '../utils/typography'
+
+class Bio extends React.Component {
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          marginBottom: rhythm(2.5),
+        }}
+      >
+        <img
+          src={profilePic}
+          alt={`Tym Butler`}
+          style={{
+            marginRight: rhythm(1 / 2),
+            marginBottom: 0,
+            width: rhythm(2),
+            height: rhythm(2),
+          }}
+        />
+        <p>
+          Written by <strong>Tym Butler</strong> who likes to create but also break things a little.{' '}
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Bio
